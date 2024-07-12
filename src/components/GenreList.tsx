ed9ce36e03f92c3c084287f6aa4e29f2faabfbf0
Filippo Topinelli <166495830+FilippoT19@users.noreply.github.com>
2024-07-12 12:12:56 +0200

    highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCroppedImageUrl from "../services/imare-url";
 
 interface Props{
     onSelectGenre : (genre:Genre) => void;
+    selectedGenre: Genre | null
 }
 
-const GenreList = ({onSelectGenre} :Props) => {
+const GenreList = ({onSelectGenre, selectedGenre} :Props) => {
   const { data, isloading, error } = useGenres();
 
   if (error) return null;
@@ -21,7 +22,7 @@ const GenreList = ({onSelectGenre} :Props) => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Button onClick={() => onSelectGenre(genre)} fontSize="lg" variant='link'>{genre.name}</Button>
+            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize="lg" variant='link'>{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
