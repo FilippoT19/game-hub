8b3d9a28b0cd9ba36d432dd1bc626f3d3f5f2d28
Filippo Topinelli <166495830+FilippoT19@users.noreply.github.com>
2024-07-12 18:03:59 +0200

    Refactor: extract query object

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,17 +4,18 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
+import { GameQuery } from "../App";
 
-interface Props{
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+interface Props {
+  gameQuery: GameQuery;
+
 }
 
-const GameGrid = ({selectedGenre, selectedPlatform} : Props) => {
-  const { data, error, isloading } = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isloading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  return ( 
+  return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
@@ -25,13 +26,13 @@ const GameGrid = ({selectedGenre, selectedPlatform} : Props) => {
         {isloading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
-              <GameCardSkeleton  />
+              <GameCardSkeleton />
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer key={game.id}> 
-          <GameCard  game={game} />
-          </GameCardContainer> 
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
